572927c610833807667c79443bbd78561a44cd67
utpalch <chanda.utpal@gmail.com>
2022-05-02 22:01:12 -0700

    Initial commit
    Initial commit

---
+++ kafka/scripts/2-kafka-server-start.sh
@@ -0,0 +1,2 @@
+#kafka-server-start.bat %KAFKA_HOME%\config\server-2.properties
+/opt/homebrew/opt/kafka/bin/kafka-server-start /opt/homebrew/etc/kafka/server-2.properties
---
+++ kafka/scripts/zookeeper-start.sh
@@ -0,0 +1,2 @@
+#zookeeper-server-start.bat %KAFKA_HOME%\config\zookeeper.properties
+/opt/homebrew/opt/kafka/bin/zookeeper-server-start /opt/homebrew/etc/kafka/zookeeper.properties
